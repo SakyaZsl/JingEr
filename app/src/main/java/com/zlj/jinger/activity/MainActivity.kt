23838fc31084b4b8d6901c98df6fdc3b2e878811
zhusiliang <zhusiliang4581@haixue.com>
2020-08-04 16:59:43 +0800

    公司电脑第二次提交

---
+++ app/src/main/java/com/zlj/jinger/activity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.zlj.jinger.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zlj.jinger.R
+
+class MainActivity :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
+
+
+    private fun initView(){
+
+    }
+}
--- app/src/main/java/com/zlj/jinger/activity/MusicActivity.kt
+++ app/src/main/java/com/zlj/jinger/activity/MusicActivity.kt
@@ -2,45 +2,30 @@ package com.zlj.jinger.activity
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.zlj.jinger.R
 import com.zlj.jinger.bean.BaseBean
-import kotlinx.android.synthetic.main.activity_music.*
 
 
 
 class MusicActivity :AppCompatActivity() {
     val TAG="zzzz"
 
+    val mMusicList= listOf("有何不可","泸州月","玫瑰花的葬礼","素颜","羡慕","清明雨上","如果当时","雅俗共享","断桥残雪","多余的解释","灰色头像","幻听")
+    val mIdealList= listOf("有何不可","泸州月","玫瑰花的葬礼","素颜","羡慕")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.zlj.jinger.R.layout.activity_eat)
+        setContentView(R.layout.activity_eat)
         initView()
     }
 
 
-    fun initView(){
-        val listener=object :View.OnClickListener{
-            override fun onClick(v: View?) {
-              Log.e("zzzz","这个是${A.C}")
-            }
-        }
-        btn_play.setOnClickListener(listener)
+    private fun initView(){
         val bean= BaseBean(18,"川建国")
         Log.e(TAG,"姓名是${bean.name}年龄是${bean.age}")
     }
 
-    override fun <T : View> findViewById(id: Int): T? {
-        return if (id == 0) {
-            this as T
-        } else null
-    }
 
-
-    class A {
-        companion object B{
-            var C:Int =10086
-        }
-    }
 
 }
